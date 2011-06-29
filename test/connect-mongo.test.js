9d49d5210cfb105dd523a542d1124513d1a77113
Casey Banner <kcbanner@gmail.com>
2011-06-28 20:11:27 -0400

    * Added expiry support

--- test/connect-mongo.test.js
+++ test/connect-mongo.test.js
@@ -9,7 +9,7 @@ var assert = require('assert');
 var options = {db: 'connect-mongo-test'};
 var mongo = require('mongodb');
 
-var open_db = function(callback) {
+var open_db = function(options, callback) {
   var store = new MongoStore(options, function() {
     var db = new mongo.Db(options.db, new mongo.Server('127.0.0.1', 27017, {}));
 
@@ -22,6 +22,7 @@ var open_db = function(callback) {
 };
 
 var cleanup_store = function(store) {
+  clearInterval(store.clear_interval);
   store.db.close();
 };
 
@@ -35,7 +36,7 @@ var cleanup = function(store, db, collection, callback) {
 };
 
 exports.test_set = function(done) {
-  open_db(function(store, db, collection) {
+  open_db(options, function(store, db, collection) {
     var sid = 'test_set-sid';
     store.set(sid, {foo:'bar'}, function(err, session) {
       assert.strictEqual(err, null);
@@ -56,8 +57,36 @@ exports.test_set = function(done) {
   });
 };
 
+exports.test_set_expires = function(done) {
+  open_db(options, function(store, db, collection) {
+    var sid = 'test_set_expires-sid';
+    var data = {
+      foo:'bar',
+      cookie:
+      {
+        _expires: '2011-04-26T03:10:12.890Z'
+      }
+    };
+    
+    store.set(sid, data, function(err, session) {
+      assert.strictEqual(err, null);
+
+      // Verify it was saved
+      collection.findOne({_id: sid}, function(err, session) {
+        assert.deepEqual(session.session, JSON.stringify(data));
+        assert.strictEqual(session._id, sid);
+        assert.equal(session.expires.toJSON(), new Date(data.cookie._expires).toJSON());
+        
+        cleanup(store, db, collection, function() {
+          done();
+        });
+      });  
+    });
+  });
+};
+
 exports.test_get = function(done) {
-  open_db(function(store, db, collection) {
+  open_db(options, function(store, db, collection) {
     var sid = 'test_get-sid';
     collection.insert({_id: sid, session: JSON.stringify({key1: 1, key2: 'two'})}, function(error, ids) {
       store.get(sid, function(err, session) {
@@ -71,7 +100,7 @@ exports.test_get = function(done) {
 };
 
 exports.test_length = function(done) {
-  open_db(function(store, db, collection) {
+  open_db(options, function(store, db, collection) {
     var sid = 'test_length-sid';
     collection.insert({_id: sid, session: JSON.stringify({key1: 1, key2: 'two'})}, function(error, ids) {
       store.length(function(err, length) {
@@ -86,11 +115,11 @@ exports.test_length = function(done) {
 };
 
 exports.test_destroy_ok = function(done) {
-  open_db(function(store, db, collection) {
+  open_db(options, function(store, db, collection) {
     var sid = 'test_destroy_ok-sid';
     collection.insert({_id: sid, session: JSON.stringify({key1: 1, key2: 'two'})}, function(error, ids) {
       store.destroy(sid, function(err) {
-        assert.strictEqual(err, null);
+        assert.strictEqual(err, undefined);
         cleanup(store, db, collection, function() {
           done();
         });
@@ -100,15 +129,17 @@ exports.test_destroy_ok = function(done) {
 };
 
 exports.test_clear = function(done) {
-  open_db(function(store, db, collection) {
+  open_db(options, function(store, db, collection) {
     var sid = 'test_length-sid';
     collection.insert({_id: sid, key1: 1, key2: 'two'}, function(error, ids) {
       store.clear(function(err) {
-        
-        
-        cleanup(store, db, collection, function() {
-          done();
-        });
+        collection.count(function(err, count) {
+          assert.strictEqual(count, 0);
+
+          cleanup(store, db, collection, function() {
+            done();
+          });
+        });        
       });
     });
   });
@@ -124,5 +155,22 @@ exports.test_options_url = function(done) {
     assert.equal(store.collection.collectionName, 'sessions-test');
     cleanup_store(store);
     done();
+  });
+};
+
+exports.test_clear_expired = function(done) {
+  open_db({db: options.db, clear_interval: 0.1}, function(store, db, collection) {
+    var sid = 'test_clear_expired-sid';
+    store.set(sid, {foo:'bar', cookie: {_expires: '2011-04-26T03:10:12.890Z'}}, function(err, session) {
+      setTimeout(function() {
+        collection.find({_id: sid}).toArray(function(err, results) {
+          assert.strictEqual(results.length, 0);
+
+          cleanup(store, db, collection, function() {
+            done();
+          });
+        });
+      }, 150);
+    });
   });
 };
