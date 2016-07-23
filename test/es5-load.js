467d21241d0bf8e3ba1d464a84f64a578efef9bc
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-07-23 17:07:53 +0200

    Start tests migration

--- test/es5-load.js
+++ test/es5-load.js
@@ -3,6 +3,6 @@
 describe('Transpiled-to-ES5 version', function () {
   it('should load with success', function () {
     const session = require('express-session');
-    require('../../es5')(session);
+    require('../es5')(session);
   });
 });
---
+++ test/legacy-loader.js
@@ -0,0 +1,8 @@
+const legacyTests = require('./legacy-tests');
+
+describe('Legacy tests', function () {
+  this.timeout(6000);
+  Object.keys(legacyTests).forEach(testName => {
+    it(testName, legacyTests[testName]);
+  });
+});
