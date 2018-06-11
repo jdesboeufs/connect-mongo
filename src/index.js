378c176d6f4c7c33be5f237e83026728fc1ade82
bGute <bGute@users.noreply.github.com>
2018-06-11 10:01:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -290,7 +290,7 @@ module.exports = function (connect) {
         })
         , callback)
     }
- 
+
     all(callback) {
       return withCallback(this.collectionReady()
         .then(collection => collection.find({
