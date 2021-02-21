7401a7a36ebe0380db1985c21919b265f3ecdc5a
mingchun.or <mingchuno@gmail.com>
2021-02-21 20:52:30 +0800

    remove all files

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', 'src'],
-  collectCoverageFrom: ['src/**/*.js', '!**/test/**', '!*.js'],
-  setupFilesAfterEnv: ['<rootDir>/test/jest.setup.js'],
-}
