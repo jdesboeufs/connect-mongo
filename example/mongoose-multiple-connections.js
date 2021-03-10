68264543fd48ae83ec31c42ae0552ed0a6392039
Aryaman1706 <agrover1_be19@thapar.edu>
2021-03-10 22:45:25 +0530

    docs(example): added an example for multiple mongoose connections

---
+++ example/mongoose-multiple-connections.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoStore = require('connect-mongo').default
+
+// App Init
+const app = express()
+const port = 3000
+
+// Mongoose Connection
+const appDBConnection = mongoose
+  .createConnection(process.env.APP_DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((connection) => {
+    console.log('Connected to AppDB.')
+    return connection
+  })
+
+const sessionDBConnection = mongoose
+  .createConnection(process.env.SESSIONS_DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((connection) => {
+    console.log('Connected to SessionsDB.')
+    return connection
+  })
+
+// Session Init
+const sessionInit = (clientPromise) => {
+  app.use(
+    session({
+      store: MongoStore.create({
+        clientPromise,
+        mongoOptions: {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        },
+      }),
+      secret: 'hello',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    })
+  )
+}
+
+// Router Init
+const router = express.Router()
+
+router.get('', (req, res) => {
+  req.session.foo = 'bar'
+  res.send('Session Updated')
+})
+
+const setup = async () => {
+  const connection = await sessionDBConnection
+  const mongoClient = connection.getClient()
+  const clientPromise = Promise.resolve(mongoClient)
+  // Session Init
+  sessionInit(clientPromise)
+  // Routes Init
+  app.use('/', router)
+}
+// prettier-ignore
+(setup())
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
